fe72d76ad107153e350af924824bf2ff8641206f
Marion Ajijala <marion.ajijala@airmconsulting.com>
2023-07-13 02:25:39 -0400

    initial commit

---
+++ build/CMakeFiles/Test.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Core/Startup/startup_stm32f072rbtx.s" "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/build/CMakeFiles/Test.dir/Core/Startup/startup_stm32f072rbtx.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Core/Src/main.c" "CMakeFiles/Test.dir/Core/Src/main.c.o" "gcc" "CMakeFiles/Test.dir/Core/Src/main.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Core/Src/stm32f0xx_hal_msp.c" "CMakeFiles/Test.dir/Core/Src/stm32f0xx_hal_msp.c.o" "gcc" "CMakeFiles/Test.dir/Core/Src/stm32f0xx_hal_msp.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Core/Src/stm32f0xx_it.c" "CMakeFiles/Test.dir/Core/Src/stm32f0xx_it.c.o" "gcc" "CMakeFiles/Test.dir/Core/Src/stm32f0xx_it.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Core/Src/syscalls.c" "CMakeFiles/Test.dir/Core/Src/syscalls.c.o" "gcc" "CMakeFiles/Test.dir/Core/Src/syscalls.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Core/Src/sysmem.c" "CMakeFiles/Test.dir/Core/Src/sysmem.c.o" "gcc" "CMakeFiles/Test.dir/Core/Src/sysmem.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Core/Src/system_stm32f0xx.c" "CMakeFiles/Test.dir/Core/Src/system_stm32f0xx.c.o" "gcc" "CMakeFiles/Test.dir/Core/Src/system_stm32f0xx.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c.o.d"
+  "/Users/marionajijala/STM32CubeIDE/workspace_1.10.1/Test/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c.o" "gcc" "CMakeFiles/Test.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
